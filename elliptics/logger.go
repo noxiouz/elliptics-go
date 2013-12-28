6a08e867efd5179deeaa4194cc0de5873c713703
Anton Tyurin <noxiouz@yandex.ru>
2013-12-28 18:28:21 +0400

    Add some docs

--- elliptics/logger.go
+++ elliptics/logger.go
@@ -1,3 +1,18 @@
+/*
+* 2013+ Copyright (c) Anton Tyurin <noxiouz@yandex.ru>
+* All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+ */
+
 package elliptics
 
 // #include "logger.h"
@@ -5,21 +20,32 @@ package elliptics
 import "C"
 
 import (
-	"unsafe"
 	"fmt"
+	"unsafe"
 )
 
 type Logger struct {
 	logger unsafe.Pointer
 }
 
+//Constants for log level of Logger.
 const (
-	ERROR = iota
-	WARNING
-	INFO
-	DEBUG
+	//Log very important data. Practically nothing is written
+	LOGDATA = iota
+	//Log critical errors that materially affect the work
+	LOGERROR
+	//Log messages about the time of the various operations
+	LOGINFO
+	//It's a first level of debugging
+	LOGNOTICE
+	//Logs all sort of information about errors and work
+	LOGDEBUG
 )
 
+/*NewFileLogger returns new Logger.
+file is path to logfile.
+level is log level.
+*/
 func NewFileLogger(file string, level int) (logger *Logger, err error) {
 	cfile := C.CString(file)
 	defer C.free(unsafe.Pointer(cfile))
@@ -41,7 +67,6 @@ func (logger *Logger) Log(level int, format string, args ...interface{}) {
 	str := fmt.Sprintf(format, args...)
 	cstr := C.CString(str)
 	defer C.free(unsafe.Pointer(cstr))
-
 
 	C.file_logger_log(logger.logger, C.int(level), cstr)
 }
--- elliptics/node.go
+++ elliptics/node.go
@@ -1,3 +1,18 @@
+/*
+* 2013+ Copyright (c) Anton Tyurin <noxiouz@yandex.ru>
+* All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+ */
+
 package elliptics
 
 // #include "node.h"
@@ -9,6 +24,7 @@ import (
 	"unsafe"
 )
 
+//A Node is responsible for the connection with the server part.
 type Node struct {
 	logger *Logger
 	node   unsafe.Pointer
@@ -21,6 +37,7 @@ func isError(errno syscall.Errno) bool {
 		errno != syscall.EISCONN
 }
 
+//NewNode returns new Node given Logger.
 func NewNode(log *Logger) (node *Node, err error) {
 	cnode, err := C.new_node(log.logger)
 	if err != nil {
@@ -30,14 +47,30 @@ func NewNode(log *Logger) (node *Node, err error) {
 	return
 }
 
+//Free disposes given Node instance.
+//Do not destroy the Node used by any Session.
 func (node *Node) Free() {
 	C.delete_node(node.node)
 }
 
+/*SetTimeouts overrides the default values for timeouts.
+
+waitTimeout affects to any transaction, which is sent to the cluster.
+Default value is 5 seconds.
+
+checkTimeout is responsible for updating the routing table
+and checking the network connection.
+By default it's 60 seconds.
+*/
 func (node *Node) SetTimeouts(waitTimeout int, checkTimeout int) {
 	C.node_set_timeouts(node.node, C.int(waitTimeout), C.int(checkTimeout))
 }
 
+/*AddRemote adds a connection to elliptics servers.
+
+Address is specified as Host:Port:Family. Family can be omitted.
+Suitable Family values are: 2 (AF_INET) and 10 (AF_INET6).
+*/
 func (node *Node) AddRemote(addr string) (err error) {
 	caddr := C.CString(addr)
 	defer C.free(unsafe.Pointer(caddr))
