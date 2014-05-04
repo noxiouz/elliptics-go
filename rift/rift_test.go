42c70594a29f3842b048d5534ef9f926dd58ea27
Anton Tyurin <noxiouz@yandex.ru>
2014-05-04 20:46:28 +0700

    Implement Rift HTTP client library into rift

---
+++ rift/rift_test.go
@@ -0,0 +1,152 @@
+package rift
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/kr/pretty"
+)
+
+var (
+	_ = fmt.Sprintf
+	_ = time.Now
+)
+
+var (
+	riftEndpoint  string
+	testDir       string        = fmt.Sprintf("testdir-%d", time.Now().Unix())
+	testBucket    string        = fmt.Sprintf("testbucket-%d", time.Now().Unix())
+	testKey       string        = fmt.Sprintf("testkey-%d", time.Now().Unix())
+	testBucketOpt BucketOptions = BucketOptions{
+		Groups:    []int{1},
+		ACL:       make([]ACLStruct, 0),
+		Flags:     0,
+		MaxSize:   0,
+		MaxKeyNum: 0,
+	}
+	testBucketDirOpt BucketDirectoryOptions = BucketDirectoryOptions{
+		Groups:    []int{1},
+		ACL:       make([]ACLStruct, 0),
+		Flags:     0,
+		MaxSize:   0,
+		MaxKeyNum: 0,
+	}
+)
+
+func init() {
+	riftEndpoint = os.Getenv("RIFT_ENDPOINT")
+}
+
+func TestEnv(t *testing.T) {
+	if riftEndpoint == "" {
+		t.Fatal("Set environment variable RIFT_ENDPOINT")
+	}
+}
+
+func TestBucketDir(t *testing.T) {
+	t.Logf("Test directory %s", testDir)
+	r, err := NewRiftClient(riftEndpoint)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	info, err := r.CreateBucketDir(testDir, testBucketDirOpt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(info.Info) == 0 {
+		t.Fatalf("Wrong info %# v", pretty.Formatter(info))
+	}
+
+	_, err = r.CreateBucket(testBucket, testDir, testBucketOpt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lst, err := r.ListBucketDirectory(testDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if keys := lst.Keys(); len(keys) == 0 || testBucket != keys[0] {
+		t.Fatalf("TestBucket is not found in listing %s %v", testBucket, lst)
+	}
+
+	err = r.DeleteBucketDirectory(testDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBucket(t *testing.T) {
+	r, err := NewRiftClient(riftEndpoint)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r.CreateBucketDir(testDir, testBucketDirOpt)
+	if err != nil {
+		t.Fatal("Create error", err)
+	}
+
+	info, err := r.CreateBucket(testBucket, testDir, testBucketOpt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(info.Info) == 0 {
+		t.Fatalf("Wrong info %# v", pretty.Formatter(info))
+	}
+
+	// listing, err := r.ListBucket(testBucket)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// t.Logf("Listing %s", listing)
+
+	// err = r.DeleteBucket(testBucket)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+}
+
+func TestObject(t *testing.T) {
+	t.Logf("Test directory %s, bucket %s, key %s", testDir, testBucket, testKey)
+	r, err := NewRiftClient(riftEndpoint)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	blob := []byte("TESTBLOB")
+
+	info, err := r.UploadObject(testBucket, testKey, blob)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(info)
+
+	value, err := r.GetObject(testBucket, testKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(value, blob) {
+		t.Fatal("value is not equal to blob %s %s", value, blob)
+	}
+
+	err = r.DeleteObject(testBucket, testKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.GetObject(testBucket, testKey)
+	if err == nil {
+		t.Fatalf("Error is expected %s", err)
+	}
+}
