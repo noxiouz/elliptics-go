2904cb2b97457b6b5250272ee193d97f566270ce
Anton Tyurin <noxiouz@yandex.ru>
2014-05-20 15:04:11 +0400

    Use pytest

--- s3_test.py
+++ s3_test.py
@@ -5,6 +5,9 @@ import time
 import boto
 import boto.s3.connection
 from boto.s3 import key
+from boto.exception import S3ResponseError
+
+import pytest
 
 suffix = "%d" % int(time.time())
 access_key = 'noxiouz'
@@ -14,34 +17,30 @@ host = "localhost"
 test_bucket = "testbucket" + suffix
 test_key = "testkey" + suffix
 
-conn = boto.connect_s3(aws_access_key_id=access_key,
-                       aws_secret_access_key=secret_key,
-                       host=host,
-                       port=9000,
-                       debug=10,
-                       is_secure=False,
-                       calling_format=boto.s3.connection.OrdinaryCallingFormat(),
-                       )
 
-print("Connect to bucket %s" % test_bucket)
-b = conn.get_bucket(test_bucket)
-b = conn.create_bucket(test_bucket)
-# k = key.Key(b)
-# k.key = 'xxx.jpg'
-# print k.exists()
-# print "SetContent %s" % k.set_contents_from_string("TEST")
-# print k.get_contents_as_string()
-# print k.set_metadata('meta1', 'This is the first metadata value')
-# print k.set_contents_from_string("push through proxy")
+class TestS3:
+    def setup_class(self):
+        self.conn = boto.connect_s3(aws_access_key_id=access_key,
+                                    aws_secret_access_key=secret_key,
+                                    host=host,
+                                    port=9000,
+                                    debug=10,
+                                    is_secure=False,
+                                    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
+                                    )
+        self.bucket = None
 
+    def test_bucket(self):
+        with pytest.raises(S3ResponseError):
+            self.conn.get_bucket(test_bucket)
+        self.bucket = self.conn.create_bucket(test_bucket)
+        self.bucket = self.conn.get_bucket(test_bucket)
 
-# possible_key = b.get_key('xxx.jpg')
-# print possible_key
-# possible_key = b.get_key('xxxdsdsdsd.jpg')
-# print possible_key
-
-# res = conn.create_bucket('mybucket')
-# print res
-
-# k = key.Key(b)
-# k.key = 'testKey'
+    def test_key(self):
+        self.bucket = self.conn.get_bucket(test_bucket)
+        k = key.Key(self.bucket)
+        k.key = test_key
+        k.set_contents_from_string("TEST")
+        assert True, k.exists()
+        possible_key = self.bucket.get_key('xxx.jpg')
+        print possible_key
