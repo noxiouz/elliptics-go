c7a0df3a018cc8e78967ffa42d00509875ebf5bf
Evgeniy Polyakov <zbr@ioremap.net>
2014-08-05 02:19:05 +0400

    node: go indent cleanup

--- elliptics/node.go
+++ elliptics/node.go
@@ -1,32 +1,29 @@
 /*
-* 2013+ Copyright (c) Anton Tyurin <noxiouz@yandex.ru>
-* All rights reserved.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
+ * 2013+ Copyright (c) Anton Tyurin <noxiouz@yandex.ru>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 package elliptics
 
-// #include "node.h"
-// #include <stdlib.h>
-import "C"
-
 import (
+	"C"
 	"syscall"
 	"unsafe"
 )
 
-//A Node is responsible for the connection with the server part.
-//Also it is responsible for checking timeouts, maintenance and checking of communication.
-//To initialize the Node you should use NewNode.
+// A Node is responsible for the connection with the server part.
+// Also it is responsible for checking timeouts, maintenance and checking of communication.
+// To initialize the Node you should use NewNode.
 type Node struct {
 	logger *Logger
 	node   unsafe.Pointer
@@ -39,7 +36,7 @@ func isError(errno syscall.Errno) bool {
 		errno != syscall.EISCONN
 }
 
-//NewNode returns new Node given Logger.
+// NewNode returns new Node given Logger.
 func NewNode(log *Logger) (node *Node, err error) {
 	cnode, err := C.new_node(log.logger)
 	if err != nil {
@@ -49,30 +46,32 @@ func NewNode(log *Logger) (node *Node, err error) {
 	return
 }
 
-//Free disposes given Node instance.
-//Do not destroy the Node used by any Session.
+// Free disposes given Node instance.
+// Do not destroy the Node used by any Session.
 func (node *Node) Free() {
 	C.delete_node(node.node)
 }
 
-/*SetTimeouts overrides the default values for timeouts.
-
-waitTimeout affects to any transaction, which is sent to the cluster.
-Default value is 5 seconds.
-
-checkTimeout is responsible for updating the routing table
-and checking the network connection.
-By default it's 60 seconds.
-*/
+/*
+ * SetTimeouts overrides the default values for timeouts.
+ *
+ * waitTimeout affects to any transaction, which is sent to the cluster.
+ * Default value is 5 seconds.
+ *
+ * checkTimeout is responsible for updating the routing table
+ * and checking the network connection.
+ * By default it's 60 seconds.
+ */
 func (node *Node) SetTimeouts(waitTimeout int, checkTimeout int) {
 	C.node_set_timeouts(node.node, C.int(waitTimeout), C.int(checkTimeout))
 }
 
-/*AddRemote adds a connection to elliptics servers.
-
-Address is specified as Host:Port:Family. Family can be omitted.
-Suitable Family values are: 2 (AF_INET) and 10 (AF_INET6).
-*/
+/*
+ * AddRemote adds a connection to elliptics servers.
+ *
+ * Address is specified as Host:Port:Family. Family can be omitted.
+ * Suitable Family values are: 2 (AF_INET) and 10 (AF_INET6).
+ */
 func (node *Node) AddRemote(addr string) (err error) {
 	caddr := C.CString(addr)
 	defer C.free(unsafe.Pointer(caddr))
