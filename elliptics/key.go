c439f2f6a6645e271ad5caa6018e3cbf6dc35128
Evgeniy Polyakov <zbr@ioremap.net>
2014-08-28 05:02:44 +0400

    keys: added keys array structure. It sorts provided data and allows to find key. Used by BulkRemove() operations

--- elliptics/key.go
+++ elliptics/key.go
@@ -22,6 +22,8 @@ package elliptics
 import "C"
 
 import (
+	"fmt"
+	"sort"
 	"unsafe"
 )
 
@@ -35,11 +37,16 @@ func NewKey(args ...interface{}) (key *Key, err error) {
 		if remote, ok := args[0].(string); ok {
 			_cRemote := C.CString(remote)
 			defer C.free(unsafe.Pointer(_cRemote))
-			ckey, err = C.new_key_remote(_cRemote)
+			ckey = C.new_key_remote(_cRemote)
+			if ckey == nil {
+				err = fmt.Errorf("could not create new key")
+				return
+			}
 		}
 	} else {
-		ckey, err = C.new_key()
-		if err != nil {
+		ckey = C.new_key()
+		if ckey == nil {
+			err = fmt.Errorf("could not create new key")
 			return
 		}
 	}
@@ -57,4 +64,54 @@ func (k *Key) ById() bool {
 
 func (k *Key) Free() {
 	C.delete_key(k.key)
+}
+
+func (k *Key) CmpID(id []uint8) int {
+	return int(C.key_id_cmp(k.key, unsafe.Pointer(&id[0])));
+}
+
+type Keys struct {
+	keys unsafe.Pointer // ell_keys structure pointer
+}
+
+func NewKeys(keys []string) (ret Keys, err error) {
+	var ckeys unsafe.Pointer
+	ckeys = C.ell_keys_new()
+	if ckeys == nil {
+		err = fmt.Errorf("could not create key array")
+		return
+	}
+
+	sort.Strings(keys)
+	for _, k := range keys {
+		ckey := C.CString(k)
+		defer C.free(unsafe.Pointer(ckey))
+		C.ell_keys_insert(ckeys, ckey, C.int(len(k)))
+	}
+
+	err = nil
+	ret = Keys {
+		keys: ckeys,
+	}
+
+	return
+}
+
+func (kk *Keys) Find(id []uint8) (ret string, err error) {
+	var tmp *C.char
+	tmp = C.ell_keys_find(kk.keys, unsafe.Pointer(&id[0]))
+	defer C.free(unsafe.Pointer(tmp))
+
+	if tmp == nil {
+		err = fmt.Errorf("could not find key for given ID")
+		return
+	}
+
+	ret = C.GoString(tmp)
+	err = nil
+	return
+}
+
+func (kk *Keys) Free() {
+	C.ell_keys_free(kk.keys)
 }
