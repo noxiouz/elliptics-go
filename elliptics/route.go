521a7ede9c3919443e7e44613e612141e42c7fc2
Evgeniy Polyakov <zbr@ioremap.net>
2015-01-10 02:58:35 +0300

    route: fixed log typo

--- elliptics/route.go
+++ elliptics/route.go
@@ -84,7 +84,7 @@ func (s *Session) GetRoutes(stat *DnetStat) {
 	context := NextContext()
 	Pool.Store(context, stat)
 
-	log.Printf("get_routes: stat: %p, condext: %d\n", stat, context)
+	log.Printf("get_routes: stat: %p, context: %d\n", stat, context)
 
 	C.session_get_routes(s.session, C.context_t(context))
 	stat.Finalize()
