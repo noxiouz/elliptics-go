fbac2bd7b0e7f411c653584d3b05da8c71fd9135
Anton Tiurin <noxiouz@yandex.ru>
2016-01-06 12:34:39 +0000

    test: migrate node tests to gocheck

--- elliptics/node_test.go
+++ elliptics/node_test.go
@@ -1,42 +1,62 @@
 package elliptics
 
 import (
-	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestNode(t *testing.T) {
-	const (
-		suitableLogLevel = "info"
-		invalidLogLevel  = "DEVNULLISHE"
+func init() {
+	Suite(&NodeSuite{})
+	Suite(&LoggerSuite{})
+}
 
-		malformedAddress = "blabla:1025:22"
-	)
+type LoggerSuite struct{}
 
-	if _, err := NewNode("/dev/stderr", invalidLogLevel); err == nil {
-		t.Errorf("NewNode: error was expected, got nil")
-	}
+func (s *LoggerSuite) TestLogLevelParsing(c *C) {
+	_, err := NewNode("/dev/stderr", "invalidloglevel")
+	c.Assert(err, ErrorMatches, "could not create node, please check stderr output")
+}
 
-	node, err := NewNode("/dev/stderr", suitableLogLevel)
-	if err != nil {
-		t.Fatalf("NewNode: unexpected error %s", err)
-	}
+type NodeSuite struct {
+	node *Node
+}
 
-	node.SetTimeouts(5, 60)
+func (s *NodeSuite) SetUpTest(c *C) {
+	node, err := NewNode("/dev/stderr", "info")
+	c.Assert(err, IsNil)
+	s.node = node
+}
 
-	if err := node.AddRemote(malformedAddress); err == nil {
-		t.Errorf("AddRemote: error was expected, but got nil")
-	}
-
-	malformedAddresses := []string{malformedAddress, malformedAddress}
-	if err := node.AddRemotes(malformedAddresses); err == nil {
-		t.Errorf("AddRemote: error was expected, but got nil")
-	}
-
-	if err := node.AddRemotes([]string{}); err == nil {
-		t.Errorf("AddRemote: error was expected, but got nil")
-	}
-
+func (s *NodeSuite) TearDownTest(c *C) {
+	// NOTE: to avoid abortion
+	/*
+		could not create new node: exception: Unknown log level: invalidloglevel
+		2016-01-06 12:09:24.239103 0000000000000000/7656/7656 INFO: Elliptics starts, flags: 0x0 [], attrs: []
+		2016-01-06 12:09:24.239879 0000000000000000/7656/7656 INFO: Grew BLOCKING pool by: 0 -> 8 IO threads, attrs: []
+		2016-01-06 12:09:24.245673 0000000000000000/7656/7656 INFO: Grew NONBLOCKING pool by: 0 -> 4 IO threads, attrs: []
+		terminate called after throwing an instance of 'boost::exception_detail::clone_impl<boost::exception_detail::error_info_injector<boost::lock_error> >'
+		  what():  boost::lock_error
+		signal: aborted
+	*/
 	time.Sleep(1 * time.Second)
-	node.Free()
+	if s.node != nil {
+		s.node.Free()
+	}
+}
+
+func (s *NodeSuite) TestNodeAddRemote(c *C) {
+	const malformedAddress = "blabla:1025:22"
+
+	err := s.node.AddRemote(malformedAddress)
+	c.Assert(err, ErrorMatches, "no such device or address")
+
+	err = s.node.AddRemotes([]string{malformedAddress, malformedAddress})
+	c.Assert(err, ErrorMatches, "invalid argument")
+
+}
+
+func (s *NodeSuite) TestAddEmptyRemotesList(c *C) {
+	err := s.node.AddRemotes([]string{})
+	c.Assert(err, ErrorMatches, "list of remotes is empty")
 }
